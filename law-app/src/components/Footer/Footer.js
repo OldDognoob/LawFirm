2835101b29e585f553843ffa23b2eddf76ef9e62
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-11 16:04:20 +0300

    footer

---
+++ law-app/src/components/Footer/Footer.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Button } from "../../globalStyles";
+import { FaFacebook } from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterSubscription,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+} from "./Footer.elements";
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to="/">Contact</FooterLink>
+            <FooterLink to="/">Support</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to="/">Submit Video</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to="/">Facebook</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon />
+            GM & PARTNERS
+          </SocialLogo>
+          <WebsiteRights>GM & PARTNERS © 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
