b9038b3c56dd7a671ed268af1901e91907160b65
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-16 21:29:00 +0300

    changes

--- law-app/src/components/Footer/Footer.js
+++ law-app/src/components/Footer/Footer.js
@@ -22,8 +22,8 @@ import {
 } from "./Footer.style";
 
 function Footer() {
-  const {register, handleSubmit, watch, errors} = useForm();
-  const onSubmit = data => {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {
     console.log(data);
   };
 
@@ -33,48 +33,32 @@ function Footer() {
     <FooterContainer>
       <FooterSubscription>
         <Form>
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <label>First name</label>
-        <input
-          type="text"
-          name="First name"
-          ref={register({ required: true, maxLength: 80 })}
-        />
-        <label>Last name</label>
-        <input
-          type="text"
-          name="Last name"
-          ref={register({ required: true, maxLength: 100 })}
-        />
-        <label>Email</label>
-        <input
-          type="text"
-          name="Email"
-          ref={register({
-            required: true,
-            pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          })}
-        />
-        <label>Mobile number</label>
-        <input
-          type="tel"
-          name="Mobile number"
-          ref={register({ required: true, maxLength: 11, minLength: 8 })}
-        />
-        <label>Query</label>
-        <input
-        name="query"
-        ref={register({ required: true, maxLength: 200})}
-      />
-        <label>Title</label>
-        <select name="Title" ref={register({ required: true })}>
-          <option value="Mr">Mr</option>
-          <option value="Mrs">Mrs</option>
-          <option value="Miss">Miss</option>
-          <option value="Dr">Dr</option>
-        </select>
-      <input type="submit" />
-    </form>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label htmlFor="firstName">First Name</label>
+              <input name="firstName" placeholder="" ref={register} />
+            </div>
+
+            <div>
+              <label htmlFor="lastName">Last Name</label>
+              <input name="lastName" placeholder="" ref={register} />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                placeholder=""
+                type="email"
+                ref={register}
+              />
+            </div>
+            <label>Query</label>
+            <input
+              name="Query"
+              ref={register({ required: true, maxLength: 200 })}
+            />
+            <input type="submit" />
+          </form>
         </Form>
       </FooterSubscription>
       <FooterLinksContainer>
@@ -119,4 +103,3 @@ function Footer() {
 }
 
 export default Footer;
-
